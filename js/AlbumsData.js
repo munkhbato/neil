1cf75a5acda3f6f84be1cbc5808de7353baa9303
munkhbato <mbsend4@gmail.com>
2021-09-19 17:11:28 +0800

    123

---
+++ js/AlbumsData.js
@@ -0,0 +1,53 @@
+const AlbumsData = [{
+    title: "World Music Traveler",
+    src: "world-traveler-cover.jpg",
+    link: "/world-music-traveler"
+  },
+  {
+    title: "Fakebook Memories",
+    src: "facebook150.jpg",
+    link: "/fakebook-memories"
+  },
+  {
+    title: "VintageGuitar I",
+    src: "vintageguitar.jpg",
+    link: "/vintage-guitar"
+  },
+  {
+    title: "Vintage Guitar II",
+    src: "vintageguitar2cover.jpg",
+    link: "/vintage-guitar-2"
+  },
+  {
+    title: "Dreams of Mexico",
+    src: "mexcover.jpg",
+    link: "/dreams-of-mexico"
+  },
+  {
+    title: "My Nova Scotia Home",
+    src: "ns-home-cover150.jpg",
+    link: "/my-novia-scotia-home"
+  },
+  {
+    title: "Princess Pukalani's Hula",
+    src: "princessthumb.jpg",
+    link: "/princess-pukalani"
+  },
+  {
+    title: "Music from the Heart",
+    src: "jacques150.jpg",
+    link: "/jacques-harosh"
+  },
+  {
+    title: "The Heavenly Melodies of Shlomo Carlebach",
+    src: "shlomocover.jpg",
+    link: "/shlomo-carlebach"
+  },
+  {
+    title: "The Heavenly Melodies of Shlomo Carlebach II",
+    src: "shlomo2150.jpg",
+    link: "/shlomo-carlebach-2"
+  },
+]
+
+export default AlbumsData
